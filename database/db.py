0ad8caeb9571d09626bc0b140a3c55f4a567361b
John Gerega <jjgerega@gmail.com>
2025-08-27 18:13:45 -0400

    Got rid of sensitive info

--- database/db.py
+++ database/db.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "Ninjago2!",
+        host = "",
+        user = "",
+        password = "",
         database = "vulcanTracker"
     )
 
